9c62b0694d8c153d08283bfc81b23f65a8d4efbf
ObjectWizard <16ohs@protonmail.com>
2024-07-24 23:06:10 +0100

    command line connect and disconnect from relays

--- src/Models/nconnection.rs
+++ src/Models/nconnection.rs
@@ -1,21 +1,25 @@
 use tokio::net::TcpStream;
-use tokio_tungstenite::{WebSocketStream, MaybeTlsStream, connect_async, tungstenite::Error };
-use log::info;
+use tokio_tungstenite::{connect_async, tungstenite::Error, MaybeTlsStream, WebSocketStream};
 
-pub struct NConnection<'a> {
-    pub uri: &'a String,
-    pub nwebsocket: WebSocketStream<MaybeTlsStream<TcpStream>>
+pub struct NConnection {
+    pub url: String,
+    pub handle: WebSocketStream<MaybeTlsStream<TcpStream>>,
 }
 
-impl<'a> NConnection<'a> {
-    pub async fn new(uri: &'a String) -> Result<Self, Error>{
-        info!("Connecting to {}", uri);
-        match connect_async(uri).await {
-            Ok((nwebsocket, _)) => { 
-                info!("Connected to {}", uri);
-                return Ok(NConnection{ uri, nwebsocket })
-                },
-            Err(e) => Err(e),
+impl NConnection {
+    pub async fn new_nconnection(uri: String) -> Result<NConnection, Error> {
+        match connect_async(&uri).await {
+            Err(e) => {
+                eprintln!("Could not connect to {} due to error: {}", &uri, e);
+                return Err(e);
+            }
+            Ok((nwebsocket, _)) => {
+                println!("Connected to {}", &uri);
+                return Ok(NConnection {
+                    url: uri,
+                    handle: nwebsocket,
+                });
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
-use log::LevelFilter;
-use env_logger;
-pub mod models;
 pub mod enums;
+pub mod models;
 pub mod services;
+use models::nconnectionmanager::NConnectionManager;
 
 #[tokio::main]
+
 async fn main() {
-    env_logger::builder().filter_level(LevelFilter::Info).init();
-    let uris = vec![String::from("wss://nostr.wine"), String::from("wss://relay.damus.io")];
-    let connectionmanager = &models::nconnectionmanager::NConnectionManager;
-    connectionmanager.connect_all(&uris).await;
-    connectionmanager.subscribe(&uris).await;
+    use std::io::{stdin, stdout, Write};
+    let mut connectionmanager = NConnectionManager::init();
+    loop {
+        print!(
+            "Currently connected to {} relays.",
+            &connectionmanager.connections.len()
+        );
+        for connection in &connectionmanager.connections {
+            print!("{}", connection.url);
+        }
+        let mut input_string = String::new();
+        print!("Type a Nostr websocket url to connect to: ");
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut input_string)
+            .expect("Incorrect string entered!");
+        if let Some('\n') = input_string.chars().next_back() {
+            input_string.pop();
+        }
+        if let Some('\r') = input_string.chars().next_back() {
+            input_string.pop();
+        }
+        let url = String::from(input_string);
+        print!("Connecting to {}", url);
+        connectionmanager.connect_new(url).await;
+    }
 }
